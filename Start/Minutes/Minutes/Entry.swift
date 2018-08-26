9fa4bb26baea68d8b3afbaed4c9abb05fd306b5c
mbudhiraja <mbudhiraja@grubhub.com>
2018-08-26 05:59:08 -0400

    added content

--- Start/Minutes/Minutes/Entry.swift
+++ Start/Minutes/Minutes/Entry.swift
@@ -2,8 +2,8 @@ import Foundation
 
 class Entry: Codable
 {
-    var title:   String = "hello"
-    var content: String = ""
+    var title:   String = "Hello"
+    var content: String = "There"
     
     var id: String = UUID().uuidString
     
